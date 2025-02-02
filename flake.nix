c21bfde5426dbfcbdcea61286c4726fb8d3b1fe6
Komo <afiqquraisyzulkarnain@gmail.com>
2025-02-02 19:59:23 +0800

    changes from nixos on Sun Feb 2 07:59:23 PM WITA 2025

--- flake.nix
+++ flake.nix
@@ -89,7 +89,7 @@
             nixGLPackages = nixGL.outputs.packages.${system};
             helixUnstable = helix.outputs.packages.${system}.helix;
             winapps = winapps.packages."${system}".winapps;
-            winapps-launcher = winapps.packages."${system}".winapps-launcher.override (prev: {
+            winapps-launcher = winapps.packages."${system}".winapps-launcher.overrideAttrs (prev: {
               patches = [ ./patches/WinAppsLauncher.patch ];
               postPatch = ''
                 substituteAllInPlace WinApps-Launcher.sh
