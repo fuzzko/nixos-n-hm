6f9d420e08333d37b0aa15631f9e49eac667365d
Komo <afiqquraisyzulkarnain@gmail.com>
2025-02-02 20:18:35 +0800

    changes from nixos on Sun Feb 2 08:18:35 PM WITA 2025

--- dotfiles/configs/winapps.nix
+++ dotfiles/configs/winapps.nix
@@ -3,8 +3,12 @@
   RDP_PASS = "MyWindowsPassword";
   RDP_DOMAIN = "";
   RDP_IP = "127.0.0.1";
-  WAFLAVOR = "podman";
   RDP_SCALE = 100;
   RDP_FLAGS = "/cert:tofu /sound /microphone /network:lan";
-  MULTIMON 
+  FREERDP_COMMAND = "";
+  WAFLAVOR = "podman";
+  MULTIMON = false;
+  DEBUG = false;
+  AUTOPAUSE = "on";
+  AUTOPAUSE_TIMEOUT = 300;
 }
