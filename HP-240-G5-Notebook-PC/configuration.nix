5d0824af3ece2a0927a710c1de836916a64d066e
Komo <afiqquraisyzulkarnain@gmail.com>
2025-02-05 18:01:53 +0800

    changes from nixos on Wed Feb 5 06:01:53 PM WITA 2025

--- HP-240-G5-Notebook-PC/configuration.nix
+++ HP-240-G5-Notebook-PC/configuration.nix
@@ -176,12 +176,20 @@ in
     openFirewall = true;
   };
 
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
+  services.pulseaudio = {
+    enable = false;
+  };
+
+  security.rtkit = {
+    enable = true;
+  };
+
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
     pulse.enable = true;
   };
 
