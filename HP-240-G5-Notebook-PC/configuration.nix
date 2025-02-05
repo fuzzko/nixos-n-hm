d4dc1dc3cf4201a245ec02350bd7fcaecb07a2c3
Komo <afiqquraisyzulkarnain@gmail.com>
2025-02-05 17:58:40 +0800

    changes from nixos on Wed Feb 5 05:58:40 PM WITA 2025

--- HP-240-G5-Notebook-PC/configuration.nix
+++ HP-240-G5-Notebook-PC/configuration.nix
@@ -84,27 +84,29 @@ in
     "::1"
   ];
 
-  services.dnscrypt-proxy2.enable = true;
-  services.dnscrypt-proxy2.settings = {
-    ipv6_servers = true;
-    require_dnssec = true;
-    sources.public-resolvers = {
-      urls = [
-        "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
-        "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      ipv6_servers = true;
+      require_dnssec = true;
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+      server_names = [
+        "mullvad-base-doh"
+        "cloudflare"
+        "nextdns"
+        "nextdns-ipv6"
       ];
-      cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
-      minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
     };
-    server_names = [
-      "mullvad-base-doh"
-      "cloudflare"
-      "nextdns"
-      "nextdns-ipv6"
-    ];
-  };
-  systemd.services.dnscrypt-proxy2.serviceConfig = {
-    StateDirectory = "dnscrypt-proxy";
+    serviceConfig = {
+      StateDirectory = "dnscrypt-proxy";
+    };
   };
 
   hardware.bluetooth.enable = true;
