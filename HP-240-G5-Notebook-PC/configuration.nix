0ffb1181f8853664a53a2579fc034dfc472e4a5c
Komo <afiqquraisyzulkarnain@gmail.com>
2025-03-16 07:36:07 +0800

    changes from nixos on Sun Mar 16 07:36:06 AM WITA 2025

--- HP-240-G5-Notebook-PC/configuration.nix
+++ HP-240-G5-Notebook-PC/configuration.nix
@@ -266,15 +266,9 @@ in
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
+      xdg-desktop-portal-kde
       xdg-desktop-portal-hyprland
     ];
-    config = {
-      common = {
-        default = [ "hyprland" "gtk" ];
-        "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
-      };
-    };
   };
 
   users.users.komo = {
