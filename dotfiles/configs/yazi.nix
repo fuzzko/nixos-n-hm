f1bff541ddc2b1885f88f0a8841b5e46022143be
Komo <afiqquraisyzulkarnain@gmail.com>
2025-03-18 05:56:58 +0800

    changes from nixos on Tue Mar 18 05:56:58 AM WITA 2025

--- dotfiles/configs/yazi.nix
+++ dotfiles/configs/yazi.nix
@@ -1,3 +1,21 @@
-{ ... }: {
-  
+{ lib, ... }:
+{
+  plugin = {
+    prepend_previewers = lib.flatten [
+      (builtins.map
+        (x: {
+          mime = "application/${x}";
+          run = "ouch";
+        })
+        [
+          "*zip"
+          "x-tar"
+          "x-bzip2"
+          "x-7z-compressed"
+          "x-rar"
+          "x-xz"
+        ]
+      )
+    ];
+  };
 }
