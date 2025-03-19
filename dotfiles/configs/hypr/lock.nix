4a2067cd632116d4afae6efd0fa30a06b92c3e0f
Komo <afiqquraisyzulkarnain@gmail.com>
2025-03-20 06:32:18 +0800

    changes from nixos on Thu Mar 20 06:32:18 AM WITA 2025

--- dotfiles/configs/hypr/lock.nix
+++ dotfiles/configs/hypr/lock.nix
@@ -11,30 +11,26 @@ in
   general = {
     hide_cursor = true;
     ignore_empty_input = true;
-    
+
   };
 
-  background = [
-    {
-      path = builtins.toString (/${root}/../resources/wallpapers/sorcerer-casting.jpg);
-      blur_passes = 1;
-    }
-  ];
+  background = {
+    path = builtins.toString (/${root}/../resources/wallpapers/sorcerer-casting.jpg);
+    blur_passes = 1;
+  };
 
-  input-field = [
-    {
-      size = "250, 55";
-      position = "0, -80";
-      dots_center = true;
-      fade_on_empty = false;
-      font_color = rgba 202 211 245 null;
-      inner_color = rgba 91 96 120 null;
-      outer_color = rgba 24 25 38 null;
-      outline_thickness = 5;
-      placeholder_text = ''
-        <span foreground="##cad3f5">Password...</span>
-      '';
-      shadow_passes = 2;
-    }
-  ];
+  input-field = {
+    size = "250, 55";
+    position = "0, -80";
+    dots_center = true;
+    fade_on_empty = false;
+    font_color = rgba 202 211 245 null;
+    inner_color = rgba 91 96 120 null;
+    outer_color = rgba 24 25 38 null;
+    outline_thickness = 2;
+    placeholder_text = ''
+      <span foreground="##cad3f5">Password...</span>
+    '';
+    shadow_passes = 2;
+  };
 }
