78e5f26806ac8cef961e84660e8803fd8739dd08
Komo <afiqquraisyzulkarnain@gmail.com>
2025-03-21 14:34:00 +0800

    changes from nixos on Fri Mar 21 02:34:00 PM WITA 2025

--- dotfiles/configs/hypr/wm.nix
+++ dotfiles/configs/hypr/wm.nix
@@ -59,16 +59,13 @@ in
 
   animations = {
     enabled = true;
-
-    bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
-
     animation = [
-      "windows, 1, 7, myBezier"
-      "windowsOut, 1, 7, default, popin 80%"
+      "windowsIn, 1, 7, default, slide top"
+      "windowsOut, 1, 7, default, slide bottom"
       "border, 1, 10, default"
       "borderangle, 1, 8, default"
       "fade, 1, 7, default"
-      "workspaces, 1, 6, default"
+      "workspaces, 1, 6, default, slidefade 20%"
     ];
   };
 
