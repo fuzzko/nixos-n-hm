37df779a5ae2b41b6e54b58500fba5709866c934
Komo <afiqquraisyzulkarnain@gmail.com>
2025-03-24 15:09:26 +0800

    changes from nixos on Mon Mar 24 03:09:25 PM WITA 2025

--- flake.nix
+++ flake.nix
@@ -100,7 +100,7 @@
               bun = prev.bun.overrideAttrs rec {
                 passthru.sources."x86_64-linux" = pkgs.fetchurl {
                   url = "https://github.com/oven-sh/bun/releases/download/bun-v${prev.bun.version}/bun-linux-x64-baseline.zip";
-                  hash = "sha256-4D6yVZphrwz7es8+WSctVWE+8UB48Vb3siUSRWyqD4s="; # update this
+                  hash = "sha256-DmHnkysLY7dBYCjaS+hHlWLha4gLOo4Htu1FfW0DIuM="; # update this
                 };
                 src = passthru.sources."x86_64-linux";
               };
