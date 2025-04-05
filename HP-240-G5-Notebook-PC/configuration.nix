0681a6f3513cad8d05089368635f4bab1dd3f6d4
Komo <afiqquraisyzulkarnain@gmail.com>
2025-04-05 12:33:37 +0800

    changes from nixos on Sat Apr 5 12:33:37 PM WITA 2025

--- HP-240-G5-Notebook-PC/configuration.nix
+++ HP-240-G5-Notebook-PC/configuration.nix
@@ -318,4 +318,12 @@ in
     enable = true;
     binfmt = true;
   };
+
+  programs.nh = {
+    enable = true;
+    clean = {
+      enable = true;
+      extraArgs = "--keep 2 --keep-since 3d";
+    };
+  };
 }
