e20689e4d5e67917833068bcc04f100a94b0227b
Komo <afiqquraisyzulkarnain@gmail.com>
2025-04-07 20:41:38 +0800

    changes from nixos on Mon Apr 7 08:41:38 PM WITA 2025

--- HP-240-G5-Notebook-PC/configs/kanata/asdf.lisp
+++ HP-240-G5-Notebook-PC/configs/kanata/asdf.lisp
@@ -1,13 +1,15 @@
 #|
   ## the asdf-jkl; thing
 
-   . [a + f] → lctrl   . [k + ;] → rctrl
-   . [a + d] → lalt    . [j + l] → ralt
-   . [a + s] → lshift  . [j + k] → rshift
-   . [s + d] → lsuper  . [k + l] → rsuper
+   . [f + d] → ctrl  . [j + k] → ctrl
+   . [s + a] → alt   . [l + ;] → alt
+   . [d + s] → shft  . [k + l] → shft
+   . [f + s] → met   . [j + l] → met
    . caps → esc
 
 |#
+
+
 
 ;; uncomment this if you're not using the NixOS option
 ; (defcfg
@@ -15,12 +17,49 @@
 
 (defsrc
   caps
-  a s d f    ;; left home row
-  j k l ";") ;; right home row
+  s d f    #| left home row  |#
+  j k l) #| right home row |#
+
+(defvirtualkey
+  relf24 (release-key f24))
 
 (defvar
   tap-time 150
   hold-time 200)
 
 (defalias
-  )
+  release-f24 (macro 5 (on-press tap-vkey relf24)))
+
+(defalias
+  s-first (multi f24 (tap-hold $tap-time $hold-time s (layer-while-held s-comb)) release-f24)
+  d-first (multi f24 (tap-hold $tap-time $hold-time d (layer-while-held d-comb)) release-f24)
+  f-first (multi f24 (tap-hold $tap-time $hold-time f (layer-while-held f-comb)) release-f24)
+
+  j-first (multi f24 (tap-hold $tap-time $hold-time j (layer-while-held j-comb)) release-f24)
+  k-first (multi f24 (tap-hold $tap-time $hold-time k (layer-while-held k-comb)) release-f24)
+  l-first (multi f24 (tap-hold $tap-time $hold-time l (layer-while-held l-comb)) release-f24))
+
+(deflayermap (f-comb)
+  d ctrl
+  s met)
+
+(deflayermap (s-comb)
+  a alt)
+
+(deflayermap (d-comb)
+  s shft)
+
+(deflayermap (j-comb)
+  k ctrl
+  l met)
+
+(deflayermap (l-comb)
+  ";" alt)
+
+(deflayermap (k-comb)
+  l shft)
+
+(deflayer base
+  esc
+  @s-first @d-first @f-first
+  @j-first @k-first @l-first)
