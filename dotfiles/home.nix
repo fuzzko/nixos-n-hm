77cdc9a7c169e2132cc827348fceccd63f6e4ddf
Komo <afiqquraisyzulkarnain@gmail.com>
2025-09-13 16:14:20 +0800

    changes from gudboye on Sat Sep 13 04:14:20 PM WITA 2025

--- dotfiles/home.nix
+++ dotfiles/home.nix
@@ -99,7 +99,8 @@ in
       openutau
       nchat
       kdePackages.kdenlive
-      bottles
+      audacity
+      igrep
       (with nur.repos."0komo"; [
         (wrapGL sklauncher)
       ])
@@ -123,6 +124,7 @@ in
   home.sessionVariables = rec {
     "XDG_PICTURES_DIR" = "${config.home.homeDirectory}/Pictures";
     "BROWSER" = "zen";
+    "IGREP_CUSTOM_EDITOR" = "hx {file_name}:{line_number}";
     "PAGER" = "moar";
     "GIT_PAGER" = PAGER;
     "SYSTEMD_PAGERSECURE" = "true";
