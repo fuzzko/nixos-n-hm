6f1dc6c418f024c824f4171749160cfe2b954d72
Komo <afiqquraisyzulkarnain@gmail.com>
2025-09-22 05:20:38 +0800

    changes from gudboye on Mon Sep 22 05:20:38 AM WITA 2025

--- dotfiles/configs/niri/utils.nix
+++ dotfiles/configs/niri/utils.nix
@@ -3,41 +3,45 @@ let
   inherit (builtins)
     listToAttrs
     ;
+
+  fromListToTaggedUnion =
+    xs:
+    listToAttrs (
+      (map (x: {
+        name = x;
+        value = a: { "${x}" = a; };
+      }) xs)
+    );
 in
 {
   # TODO: replace `matches.*` with smth like `matches { app-id = ...; title = [ ... ]; }`
 
-  # attrs of lambda that generates an attr for `matches` option
-  window-rules.matches = listToAttrs (
-    (map
-      (name: {
-        inherit name;
-        value = x: { "${name}" = x; };
-      })
-      [
-        "app-id"
-        "title"
-        "is-active"
-        "is-active-in-column"
-        "is-floating"
-        "is-focused"
-        "is-urgent"
-        "is-window-cast-target"
-        "at-startup"
-      ]
-    )
-  );
+  animations.kind = fromListToTaggedUnion [
+    "easing"
+    "spring"
+  ];
+
+  # lambdas to create a tagged union for `matches` option
+  window-rules.matches = fromListToTaggedUnion [
+    "app-id"
+    "title"
+    "is-active"
+    "is-active-in-column"
+    "is-floating"
+    "is-focused"
+    "is-urgent"
+    "is-window-cast-target"
+    "at-startup"
+  ];
 
   spawn-at-startup = {
     # wrapper for `argv`, behaves like actions.spawn
     spawn = {
       argv = [ ];
-      __functor =
-        self: arg:
-        {
-          inherit (self) __functor;
-          argv = self.argv ++ [arg];
-        };
+      __functor = self: arg: {
+        inherit (self) __functor;
+        argv = self.argv ++ [ arg ];
+      };
     };
     # wrapper for `sh`, behaves like actions.spawn-sh
     spawn-sh = sh: { inherit sh; };
