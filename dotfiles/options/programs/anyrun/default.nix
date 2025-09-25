d9192e216352f7d927974a4d053c7196dcf0ed1f
Komo <afiqquraisyzulkarnain@gmail.com>
2025-09-25 20:14:09 +0800

    changes from gudboye on Thu Sep 25 08:14:09 PM WITA 2025

--- dotfiles/options/programs/anyrun/default.nix
+++ dotfiles/options/programs/anyrun/default.nix
@@ -1,7 +1,17 @@
-{ ... }:
+{ config, lib, ... }:
+let
+  pkg = config.programs.anyrun.package;
+in
 {
   programs.anyrun.enable = true;
   programs.anyrun.extraCss = ''
-      @import url("${toString ./anyrun.css}");
-    '';
+    @import url("${toString ./anyrun.css}");
+  '';
+  programs.anyrun.config = {
+    plugins = [
+      "${pkg}/lib/libapplications.so"
+      "${pkg}/lib/libsymbols.so"
+    ]
+    ++ lib.optional (config.services.kidex.enable) "${pkg}/lib/libkidex.so";
+  };
 }
