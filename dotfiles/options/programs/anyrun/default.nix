83960455d4ecb7bf4c4b13e876c1121397d47cd0
Komo <afiqquraisyzulkarnain@gmail.com>
2025-09-26 03:39:37 +0800

    changes from gudboye on Fri Sep 26 03:39:37 AM WITA 2025

--- dotfiles/options/programs/anyrun/default.nix
+++ dotfiles/options/programs/anyrun/default.nix
@@ -11,7 +11,12 @@ in
     plugins = [
       "${pkg}/lib/libapplications.so"
       "${pkg}/lib/libsymbols.so"
+      "${pkg}/lib/libshell.so"
+      "${pkg}/lib/librink.so"
+      "${pkg}/lib/libwebsearch.so"
+      "${pkg}/lib/libnix_run.so"
     ]
-    ++ lib.optional config.services.kidex.enable "${pkg}/lib/libkidex.so";
+    ++ lib.optional config.services.kidex.enable "${pkg}/lib/libkidex.so"
+    ++ lib.optional config.programs.niri.enable "${pkg}/lib/libniri_focus.so";
   };
 }
