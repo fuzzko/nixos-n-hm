adf41061079b716e64e294d0dc7f5370a370331d
Komo <afiqquraisyzulkarnain@gmail.com>
2025-09-29 18:12:20 +0800

    changes from gudboye on Mon Sep 29 06:12:20 PM WITA 2025

--- dotfiles/options/programs/foot/default.nix
+++ dotfiles/options/programs/foot/default.nix
@@ -1,6 +1,15 @@
 { ... }:
 {
   enable = true;
-  settings = loadConfig' "foot" { };
+  settings = {
+    main = {
+      shell = "fish";
+      login-shell = true;
+
+      font = "DepartureMono Nerd Font Mono:size=10";
+    };
+    bell.system = false;
+  };
+
   server.enable = true;
 }
