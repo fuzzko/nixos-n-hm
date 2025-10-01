8d95edd4499a58b4be81199c239b5b0275fefc71
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-02 04:51:23 +0800

    changes from gudboye on Thu Oct 2 04:51:22 AM WITA 2025

---
+++ dotfiles/options/nix/default.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }: {  
+  nix = {
+    package = pkgs.lixPackageSets.latest.lix;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      allowed-users = [ config.home.username ];
+      auto-optimise-store = true;
+    };
+    registry.nixpkgs = {
+      from = {
+        id = "nixpkgs";
+        type = "indirect";
+      };
+      to = {
+        owner = "NixOS";
+        repo = "nixpkgs";
+        ref = (builtins.fromJSON (builtins.readFile ../flake.lock)).nodes.nixpkgs.locked.rev;
+        type = "github";
+      };
+    };
+  };
+}
