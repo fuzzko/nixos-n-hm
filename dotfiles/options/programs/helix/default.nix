881aa282ac1d5a759673c434685448cf323553c1
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-02 04:56:31 +0800

    changes from gudboye on Thu Oct 2 04:56:31 AM WITA 2025

--- dotfiles/options/programs/helix/default.nix
+++ dotfiles/options/programs/helix/default.nix
@@ -15,7 +15,94 @@
         fnlfmt
       ]
     );
-    languages = loadConfig "helix/languages" { };
+  };
+
+  programs.helix.languages = {
+    language-server.nil = {
+      config = {
+        nil.formatting.command = [ "nixfmt" ];
+      };
+    };
+
+    language-server.emmet-langserver = {
+      command = "emmet-language-server";
+      args = [ "--stdio" ];
+    };
+    language-server.superhtml = {
+      command = "superhtml";
+      args = [ "lsp" ];
+    };
+    language-server.tailwindcss-langserver = {
+      command = "tailwindcss-language-server";
+      args = [ "--stdio" ];
+      required-root-patterns = [ "tailwind.config.js" ];
+    };
+
+    language =
+      let
+        indent = {
+          unit = "  ";
+          tab-width = 2;
+        };
+      in
+      [
+        {
+          name = "html";
+          roots = [ ".git" ];
+          language-servers = [
+            "emmet-langserver"
+            "superhtml"
+          ];
+        }
+        {
+          inherit indent;
+
+          name = "civet";
+          scope = "source.civet";
+          file-types = [ "civet" ];
+          comment-tokens = "//";
+          block-comment-tokens = [
+            {
+              start = "/*";
+              end = "*/";
+            }
+            {
+              start = "###\n";
+              end = "\n###";
+            }
+          ];
+        }
+        {
+          inherit indent;
+
+          name = "nelua";
+          scope = "source.nelua";
+          file-types = [ "nelua" ];
+          comment-tokens = "--";
+          block-comment-tokens = {
+            start = "--[[";
+            end = "]]";
+          };
+        }
+        {
+          inherit indent;
+
+          name = "c";
+        }
+      ];
+
+    grammar = [
+      {
+        name = "fennel";
+        source = {
+          path =
+            (builtins.fetchGit {
+              url = "https://github.com/0komo/tree-sitter-fennel.git";
+              ref = "merged";
+            }).outPath;
+        };
+      }
+    ];
   };
 
   programs.helix.settings = {
