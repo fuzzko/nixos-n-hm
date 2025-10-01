e50c7f647b035f543757c86af366fff3bb9d0b17
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-02 04:53:57 +0800

    changes from gudboye on Thu Oct 2 04:53:57 AM WITA 2025

--- dotfiles/options/services/git-sync/default.nix
+++ dotfiles/options/services/git-sync/default.nix
@@ -4,11 +4,11 @@
   services.git-sync.repositories = {
     nix = {
       path = "${config.home.homeDirectory}/nix";
-      uri = "https://github.com/mbekkomo/nix.git";
+      uri = "https://github.com/0komo/nix.git";
     };
     password-store = {
       path = "${config.home.homeDirectory}/.local/share/password-store";
-      uri = "https://github.com/mbekkomo/.password-store";
+      uri = "https://github.com/0komo/.password-store";
     };
   };
 }
