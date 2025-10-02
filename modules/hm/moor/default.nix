28b7d0f1ae1b372bf21a0cb93da06c981952ef96
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-03 03:15:04 +0800

    changes from gudboye on Fri Oct 3 03:15:04 AM WITA 2025

--- modules/hm/moor/default.nix
+++ modules/hm/moor/default.nix
@@ -1,15 +1,31 @@
 { config, lib, pkgs, ... }:
-let
-  cfg = config.programs.moor;
-in
 {
-  programs.moor = {
+  options.programs.moor = {
     enable = lib.mkEnableOption "moor";
     package = lib.mkPackageOption pkgs "moor" {
       description = "Which package to use for installing moor";
+      nullable = true;
     };
     extraOptions = lib.mkOption {
       type = with lib.types; listOf str;
+      description = "Extra command line options passed to moor";
+      default = [];
+      example = [
+        "--statusbar=bold"
+        "--no-linenumbers"
+      ];
     };
   };
+
+  config =
+    let
+      cfg = config.programs.moor;
+    in
+    lib.mkIf cfg.enable {
+      home.sessionVariables = {
+        "MOOR" = lib.concatStringsSep " " cfg.extraOptions;
+      };
+
+      home.packages = lib.mkIf (cfg.package != null) [ cfg.package ];
+    };
 }
