64dcc99eb16abbc47e714aa33c72764a1633f1f4
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-03 04:06:26 +0800

    changes from gudboye on Fri Oct 3 04:06:26 AM WITA 2025

---
+++ dotfiles/options/programs/moor/default.nix
@@ -0,0 +1,22 @@
+{ config, lib, ... }:
+let
+  cfg = config.programs.moor;
+in
+{
+  programs.moor.enable = true;
+  programs.moor.extraOptions = [
+    "-reformat"
+    "-render-unprintable highlight"
+    "-shift 10"
+    "-style autumn"
+    "-wrap"
+  ];
+
+  programs.git.extraConfig = {
+    core.pager = config.home.sessionVariables.PAGER;
+  };
+
+  home.sessionVariables = {
+    "PAGER" = lib.mkDefault cfg.package.meta.mainProgram;
+  };
+}
