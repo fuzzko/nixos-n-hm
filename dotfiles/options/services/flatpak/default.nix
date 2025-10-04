14e8702fc49d795d58d156fb9d219fe95052936f
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-04 13:58:33 +0800

    changes from gudboye on Sat Oct 4 01:58:33 PM WITA 2025

--- dotfiles/options/services/flatpak/default.nix
+++ dotfiles/options/services/flatpak/default.nix
@@ -2,14 +2,35 @@
 {
   services.flatpak.enable = true;
   services.flatpak = {
-    packages = [
-      "io.itch.itch"
-      "org.vinegarhq.Sober"
-      "com.usebottles.bottles"
-    ];
     update.auto = {
       enable = true;
+      onCalendar = "weekly";
     };
     uninstallUnmanaged = true;
   };
+
+  services.flatpak.overrides = {
+    global = {
+      # force some apps to use wayland
+      Context.sockets = [
+        "fallback-x11"
+        "!x11"
+        "wayland"
+      ];
+
+      Environment = {
+        # Fix un-themed cursor in some Wayland apps
+        XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
+
+        # Force correct theme for some GTK apps
+        GTK_THEME = "Adwaita:dark";
+      };
+    };
+  };
+
+  services.flatpak.packages = [
+    "io.itch.itch"
+    "org.vinegarhq.Sober"
+    "com.usebottles.bottles"
+  ];
 }
