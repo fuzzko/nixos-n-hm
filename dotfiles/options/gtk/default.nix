7bdec2e74ce68b416b5d404ecf5b73a1ae6566d4
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-04 21:03:31 +0800

    changes from gudboye on Sat Oct 4 09:03:31 PM WITA 2025

--- dotfiles/options/gtk/default.nix
+++ dotfiles/options/gtk/default.nix
@@ -6,10 +6,10 @@
   };
   
   gtk.theme = {
-    theme = "Adwaita";
+    theme = "Adwaita-dark";
   };
   gtk.gtk3.theme = {
     package = pkgs.adw-gtk3;
-    theme = "adw-gtk3";
+    theme = "adw-gtk3-dark";
   };
 }
