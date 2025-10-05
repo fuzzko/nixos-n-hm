89d9c16ffcc165083ac81fe8439dcb8f24080df0
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-05 08:00:05 +0800

    changes from gudboye on Sun Oct 5 08:00:05 AM WITA 2025

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -375,6 +375,13 @@ in
   };
 
   hardware.i2c.enable = true;
+
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness"
+    ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"
+    ACTION=="add", SUBSYSTEM=="leds", RUN+="/bin/chgrp video /sys/class/leds/%k/brightness"
+    ACTION=="add", SUBSYSTEM=="leds", RUN+="/bin/chmod g+w /sys/class/leds/%k/brightness"
+  '';
   
   programs.niri.enable = true;
   programs.niri.package = pkgs.niri-unstable;
