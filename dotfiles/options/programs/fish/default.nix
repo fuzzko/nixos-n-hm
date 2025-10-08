5c913252616fa2193eebd90aa4e4156fb05cb87c
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-08 22:09:35 +0800

    changes from gudboye on Wed Oct 8 10:09:35 PM WITA 2025

--- dotfiles/options/programs/fish/default.nix
+++ dotfiles/options/programs/fish/default.nix
@@ -47,8 +47,8 @@ in
     '';
 
     fish_title = ''
-    set -q argv[1]; or set argv fish
-    echo (fish_prompt_pwd_dir_length=1 prompt_pwd): $argv;
+      set -q argv[1]; or set argv fish
+      echo (fish_prompt_pwd_dir_length=1 prompt_pwd): $argv;
     '';
   };
 
