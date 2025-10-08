4692439b419ce189fdde8c6cf69a9bbe1cb32160
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-08 22:01:02 +0800

    changes from gudboye on Wed Oct 8 10:01:02 PM WITA 2025

--- dotfiles/options/programs/fish/default.nix
+++ dotfiles/options/programs/fish/default.nix
@@ -15,28 +15,8 @@ let
     };
 in
 {
+  programs.fish.enable = true;
   programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      zoxide init fish | source
-      source ${builtins.toString ./etc/lscolors.fish}
-
-      set fzf_preview_dir_cmd "eza --all --color=always --icons=always"
-
-      set fish_cursor_default block
-      set fish_cursor_insert line
-      set fish_cursor_replace_one underscore
-      set fish_cursor_replace underscore
-      set fish_cursor_visual block
-
-      # disable the stupid autosuggestion
-      set fish_autosuggestion_enabled 0
-
-      fish_default_key_bindings -M insert
-      fish_vi_key_bindings --no-erase insert
-
-      alias ftodo "ig TODO"
-    '';
     plugins =
       map
         (x: {
@@ -56,4 +36,24 @@ in
           ]
         );
   };
+
+  programs.fish.interactiveShellInit = ''
+    source ${lsColors}/lscolors.csh
+
+    set fzf_preview_dir_cmd "eza --all --color=always --icons=always"
+
+    set fish_cursor_default block
+    set fish_cursor_insert line
+    set fish_cursor_replace_one underscore
+    set fish_cursor_replace underscore
+    set fish_cursor_visual block
+
+    # disable the stupid autosuggestion
+    set fish_autosuggestion_enabled 0
+
+    fish_default_key_bindings -M insert
+    fish_vi_key_bindings --no-erase insert
+
+    alias ftodo "ig TODO"
+  '';
 }
