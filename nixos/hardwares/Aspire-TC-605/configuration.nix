777ce988a1a32d8a4d17e789d6caeab30ae69660
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-20 15:38:20 +0800

    changes from gudboye on Mon Oct 20 03:38:18 PM WITA 2025

--- nixos/hardwares/Aspire-TC-605/configuration.nix
+++ nixos/hardwares/Aspire-TC-605/configuration.nix
@@ -1,10 +1,6 @@
 { lib, ... }: {
   services.kanata.enable = lib.mkForce false;
 
-  boot.kernelParams = [
-    "ath9k.nohwcrypt=1"
-  ];
-
   networking.networkmanager.settings = {
     device = {
       match-device = "interface-name:wlan0";
