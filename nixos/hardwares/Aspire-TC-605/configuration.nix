089aefa00049a44216b2ff278077d24cfa851b5d
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-20 16:52:08 +0800

    changes from gudboye on Mon Oct 20 04:52:08 PM WITA 2025

--- nixos/hardwares/Aspire-TC-605/configuration.nix
+++ nixos/hardwares/Aspire-TC-605/configuration.nix
@@ -1,11 +1,3 @@
 { lib, ... }: {
   services.kanata.enable = lib.mkForce false;
-
-  networking.networkmanager.settings = {
-    device = {
-      match-device = "interface-name:wlan0";
-      carrier-wait-timeout = 10000;
-      ignore-carrier = true;
-    };
-  };
 }
