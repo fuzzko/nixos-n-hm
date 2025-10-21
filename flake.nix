4cf1f567022e859cae5af788098a1cd439354439
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-21 21:43:02 +0800

    changes from gudboye on Tue Oct 21 09:43:02 PM WITA 2025

--- flake.nix
+++ flake.nix
@@ -21,6 +21,7 @@
     kidex.url = "github:Kirottu/kidex";
     wired.url = "github:Toqozz/wired-notify";
     ls_colors.url = "github:trapd00r/LS_COLORS";
+    stylix.url = "github:nix-community/stylix";
 
     nur.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -39,6 +40,7 @@
     };
     wired.inputs.nixpkgs.follows = "nixpkgs";
     ls_colors.flake = false;
+    stylix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   nixConfig = {
