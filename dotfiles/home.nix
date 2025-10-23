a7e9f64e7576fde5e6541495d378388d44d21586
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-24 03:38:10 +0800

    changes from gudboye on Fri Oct 24 03:38:10 AM WITA 2025

--- dotfiles/home.nix
+++ dotfiles/home.nix
@@ -18,121 +18,11 @@ in
     ../modules/hm/moor
   ];
 
-  programs.home-manager.enable = true;
-  home = {
-    username = "komo";
-    homeDirectory = /home/${config.home.username};
-  };
-  home.stateVersion = "24.05"; # do not change
-
-  home.packages = lib.flatten (
-    with pkgs;
-    [
-      # Fonts
-      (with nerd-fonts; [
-        terminess-ttf
-      ])
-      noto-fonts
-      noto-fonts-color-emoji
-
-      # Nix-purpose
-      nix-search
-      cachix
-
-      # Misc.
-      micro
-      moar
-      glow
-      libnotify
-      unzip
-      ouch
-
-      # Something that would ease me off
-      riff
-      khal
-      fzf
-      ripgrep
-      fd
-      sd
-      zoxide
-      bat
-      sigi
-      gum
-      xdragon
-      obsidian
-
-      # For hyprland
-      nemo
-      nemo-preview
-      nemo-fileroller
-      hyprshot
-      hyprpicker
-      hyprsunset
-      hyprsysteminfo
-      playerctl
-      wireplumber
-      clipse
-      wl-clipboard
-      nushell
-      brightnessctl
-
-      # For CwC
-      app2unit
-
-      # For recording
-      kooha
-      scrcpy
-      mumble
-
-      # i don't fucking know why i installed these shits but i installed them anyway
-      youtube-music
-      qview
-      openutau
-      nchat
-      kdePackages.kdenlive
-      audacity
-      igrep
-      packet
-      (with nur.repos."0komo"; [
-        (wrapGL sklauncher)
-      ])
-    ]
-  );
-
-  home.file =
-    # External Config
-    {
-      ".config/zls.json".text = builtins.toJSON (loadConfig "zls" { });
-      ".config/walker/config.json".text = builtins.toJSON (loadConfig "walker" { });
-      ".config/luakit" = {
-        source = ./configs/luakit;
-        recursive = true;
-      };
-      # Misc. files
-      ".config/fish/functions/nixs.fish".source = ./shells/nixs.fish;
-      ".config/fish/functions/nixd.fish".source = ./shells/nixd.fish;
-    };
-
-  home.sessionVariables = rec {
-    "XDG_PICTURES_DIR" = "${config.home.homeDirectory}/Pictures";
-    "BROWSER" = "zen";
-    "IGREP_CUSTOM_EDITOR" = "hx {file_name}:{line_number}";
-    "PAGER" = "moar";
-    "GIT_PAGER" = PAGER;
-    "SYSTEMD_PAGERSECURE" = "true";
-  };
-
   home.pointerCursor.enable = true;
   home.pointerCursor.size = 15;
   home.pointerCursor = {
     hyprcursor.enable = true;
     x11.enable = true;
-  };
-
-  nixGL = {
-    packages = pkgs.nixGLPackages;
-    defaultWrapper = "mesa";
-    installScripts = [ "mesa" ];
   };
 
   programs.nix-index.enable = true;
@@ -164,13 +54,4 @@ in
     settings = loadConfig "hypr/wm" { };
     xwayland.enable = true;
   };
-
-  services.arrpc = {
-    enable = true;
-  };
-
-  caches.cachix = [
-    "nix-community"
-    "0komo"
-  ];
 }
