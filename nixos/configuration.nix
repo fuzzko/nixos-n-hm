62e67ae74ff5f89c79fc8a7cad1d69c8154521de
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-23 22:37:01 +0800

    changes from gudboye on Thu Oct 23 10:37:01 PM WITA 2025

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -170,7 +170,6 @@ in
   networking.networkmanager = {
     wifi.backend = "iwd";
     wifi.powersave = false;
-    wifi.scanRandMacAddress = false;
   };
 
   networking.networkmanager.settings = {
