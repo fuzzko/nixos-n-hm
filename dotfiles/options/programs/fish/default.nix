7b1e010e6983ece1c8d074baacd3bd8b368434fe
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-23 21:00:47 +0800

    changes from gudboye on Thu Oct 23 09:00:47 PM WITA 2025

--- dotfiles/options/programs/fish/default.nix
+++ dotfiles/options/programs/fish/default.nix
@@ -195,6 +195,7 @@ in
   ];
 
   programs.fzf.enable = true;
+  programs.fzf.defaultCommand = "fd --type f";
   programs.fzf.defaultOptions = [
     "--reverse"
     "--cycle"
@@ -204,4 +205,8 @@ in
     "--input-border=bold"
   ];
   programs.fzf.fileWidgetCommand = "fd --type f --strip-cwd-prefix --hidden --follow --exclude .git";
+  programs.fzf.fileWidgetOptions = [
+    "--list-border=none"
+    "--input-border=line"
+  ];
 }
