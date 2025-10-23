f85468013087fd56d3c67629bef65f3b03b7073c
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-24 03:12:50 +0800

    changes from gudboye on Fri Oct 24 03:12:50 AM WITA 2025

--- dotfiles/options/default.nix
+++ dotfiles/options/default.nix
@@ -1,7 +1,12 @@
 # any options that does not need to have it's own dir
-{ ... }: {
+{ pkgs, ... }: {
   caches.cachix = [
     "nix-community"
     "0komo"
   ];
+
+  nixGL = {
+    packages = pkgs.nixGLPackages;
+    installScripts = [ "mesa" ];
+  };
 }
