00ec57a09cbe7065e9a4de2038aab9be4fabde68
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-25 22:21:29 +0800

    changes from gudboye on Sat Oct 25 10:21:29 PM WITA 2025

--- dotfiles/options/services/hypridle/default.nix
+++ dotfiles/options/services/hypridle/default.nix
@@ -15,37 +15,41 @@
       '';
     };
 
-    listener = [
-      {
-        timeout = 150;
-        on-timeout = ''
-          brightnessctl -s set 10
-        '';
-        on-resume = ''
-          brightnessctl -r
-        '';
-      }
-      {
-        timeout = 300;
-        on-timeout = ''
-          loginctl lock-session
-        '';
-      }
-      {
-        timeout = 330;
-        on-timeout = ''
-          hyprctl dispatch dpms off
-        '';
-        on-resume = ''
-          hyprctl dispatch dpms on
-        '';
-      }
-      {
-        timeout = 1800;
-        on-timeout = ''
-          systemctl suspend
-        '';
-      }
-    ];
+    listener =
+      let
+        mkListener = timeout: set: { inherit timeout; } // set;
+      in
+      [
+        {
+          timeout = 150;
+          on-timeout = ''
+            brightnessctl -s set 10
+          '';
+          on-resume = ''
+            brightnessctl -r
+          '';
+        }
+        {
+          timeout = 300;
+          on-timeout = ''
+            loginctl lock-session
+          '';
+        }
+        {
+          timeout = 330;
+          on-timeout = ''
+            hyprctl dispatch dpms off
+          '';
+          on-resume = ''
+            hyprctl dispatch dpms on
+          '';
+        }
+        {
+          timeout = 1800;
+          on-timeout = ''
+            systemctl suspend
+          '';
+        }
+      ];
   };
 }
