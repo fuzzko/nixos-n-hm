9ebbfc2f10b4208fbffa61775abd445d4c153840
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-26 20:40:52 +0800

    changes from gudboye on Sun Oct 26 08:40:51 PM WITA 2025

--- dotfiles/home.nix
+++ dotfiles/home.nix
@@ -1,16 +1,9 @@
 {
   config,
-  pkgs,
-  lib,
   ...
-}@inputs:
+}:
 let
-  wrapGL = config.lib.nixGL.wrap;
-
   inherit (config.lib) komo;
-
-  loadConfig = x: y: import ./configs/${x}.nix (inputs // { root = ./.; } // y);
-  loadConfig' = x: y: import ./configs/${x} (inputs // { root = ./.; } // y);
 in
 {
   # A simple business logic to import all configs in ./options, you should check that dir too
