207b36f104cd778d7ce4751799eb3e1545749341
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-28 19:15:00 +0800

    changes from gudboye on Tue Oct 28 07:15:00 PM WITA 2025

---
+++ dotfiles/options/programs/hyprlock/default.nix
@@ -0,0 +1,69 @@
+{ ... }: {
+  programs.hyprlock.enable = true;
+
+  programs.hyprlock.settings = 
+{
+  general = {
+    immediate_render = true;
+    ignore_empty_input = true;
+    hide_cursor = true;
+  };
+
+  background = {
+    monitor = "";
+    path = "screenshot";
+    blur_passes = 2;
+  };
+
+  input-field = {
+    monitor = "";
+    size = "20%, 7%";
+    dots_size = 0.28;
+    dots_spacing = 0.3;
+    outline_thickness = 0;
+    rounding = 5;
+    fade_on_empty = false;
+    outer_color = "$base";
+    inner_color = "$overlay1";
+    font_color = "$mantle";
+    fail_color = "$red";
+    check_color = "$blue";
+    position = "0, -30%";
+    shadow_passes = 1;
+    shadow_size = 1;
+    font_family = "Terminess Nerd Font";
+    dots_text_format = "*";
+    placeholder_text = ''
+      <span foreground="##$surface2Alpha"><i>Password...</i></span>
+    '';
+  };
+
+  label =
+    let
+      date_cmd = "${pkgs.nushell}/bin/nu ${toString ./bin/date.nu}";
+    in
+    builtins.map (v: v // { monitor = ""; }) [
+      {
+        text = ''
+          cmd[update:1000] ${date_cmd} clock
+        '';
+        text_align = "left";
+        color = "$text";
+        font_family = "Terminess Nerd Font";
+        font_size = 27;
+        shadow_passes = 2;
+        position = "1.8%, 5.8%";
+      }
+      {
+        text = ''
+          cmd[update:0:1] ${date_cmd} date
+        '';
+        text_align = "left";
+        color = "$text";
+        font_family = "Terminess Nerd Font";
+        font_size = 20;
+        shadow_passes = 2;
+        position = "1.8%, 1.8%";
+      }
+    ];
+};}
