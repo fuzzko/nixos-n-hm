96fe1deffb401d1f4114caa4ed1bb10faee73543
Komo <afiqquraisyzulkarnain@gmail.com>
2025-10-28 21:33:52 +0800

    changes from gudboye on Tue Oct 28 09:33:52 PM WITA 2025

--- dotfiles/home.nix
+++ dotfiles/home.nix
@@ -10,26 +10,4 @@ in
   import = (komo.filterFilesInDir (x: (builtins.baseNameOf x) == "default.nix") ./options) ++ [
     ../modules/hm/moor
   ];
-
-  services.hypridle = {
-    enable = true;
-    settings = loadConfig "hypr/idle" { };
-  };
-
-  services.hyprpaper = {
-    enable = true;
-    settings = loadConfig "hypr/paper" { };
-  };
-
-  programs.hyprlock = {
-    enable = true;
-    settings = loadConfig "hypr/lock" { };
-  };
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-    package = null;
-    settings = loadConfig "hypr/wm" { };
-    xwayland.enable = true;
-  };
 }
