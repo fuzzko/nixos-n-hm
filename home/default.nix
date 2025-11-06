5787edab67d68e9e46a86c1e59a6f9f65d535a7b
Komo <afiqquraisyzulkarnain@gmail.com>
2025-11-06 20:17:30 +0800

    changes from gudboye on Thu Nov 6 08:17:30 PM WITA 2025

--- home/default.nix
+++ home/default.nix
@@ -2,11 +2,12 @@
   # pkgs,
   # config,
   ...
-}:
+}@attrs:
 # let
 #   inherit (config.lib) komo;
 # in
 # assert builtins.trace (builtins.typeOf pkgs) false;
+assert builtins.trace attrs true;
 {
   # # A simple business logic to import all configs in ./options, you should check that dir too
   # imports = (komo.filterFilesInDir (x: (builtins.baseNameOf x) == "default.nix") ./options) ++ [
