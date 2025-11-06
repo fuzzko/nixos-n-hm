fc5aa9362f457e10f0870058ae73e41aef9ba738
Komo <afiqquraisyzulkarnain@gmail.com>
2025-11-06 20:09:36 +0800

    changes from gudboye on Thu Nov 6 08:09:36 PM WITA 2025

--- flake.nix
+++ flake.nix
@@ -127,7 +127,20 @@
             kidex.homeModules.kidex
             wired.homeManagerModules.default
             ./home/options/lib/komo
-            ./home
+            (
+              let
+                inherit (config.lib) komo;
+              in
+              {
+                # A simple business logic to import all configs in ./options, you should check that dir too
+                imports = (komo.filterFilesInDir (x: (builtins.baseNameOf x) == "default.nix") ./options) ++ [
+                  ../modules/hm/moor
+                ];
+
+                home.stateVersion = "24.05";
+              }
+            )
+
           ];
           extraSpecialArgs.std = nix-std.lib;
         };
