7c1cb11b70a02f7db9056c9ed92be68c8407b8f2
Komo <afiqquraisyzulkarnain@gmail.com>
2025-11-06 13:17:53 +0800

    changes from gudboye on Thu Nov 6 01:17:53 PM WITA 2025

--- home/options/programs/fish/default.nix
+++ home/options/programs/fish/default.nix
@@ -32,6 +32,12 @@ in
       puffer
       fzf-fish
       forgit
+      (pkgs.fetchFromGitHub {
+        owner = "sshilovsky";
+        repo = "fish-helix";
+        rev = "8a5c7999ec67ae6d70de11334aa888734b3af8d7";
+        hash = "sha256-04cL9/m5v0/5dkqz0tEqurOY+5sDjCB5mMKvqgpV4vM=";
+      })
     ]
   );
 
