c92f20d026aaaf72761d20004156d1c1bc8538af
Komo <afiqquraisyzulkarnain@gmail.com>
2025-11-09 15:36:46 +0800

    changes from gudboye on Sun Nov 9 03:36:46 PM WITA 2025

--- home/options/programs/hyprlock/default.nix
+++ home/options/programs/hyprlock/default.nix
@@ -1,25 +1,49 @@
 { config, ... }:
 let
   inherit (config.lib) komo;
+  inherit (komo.hypr) percentXY;
+
+  quadton = a: b: c: d: [
+    a
+    b
+    c
+    d
+  ];
 in
 {
   programs.hyprlock.enable = true;
 
   programs.hyprlock.settings = {
     animation.bezier = komo.hypr.mkBeziers {
-      easeOutExpo = [
-        0.16
-        1
-        0.3
-        1
-      ];
-      easeOutCirc = [
-        0
-        0.55
-        0.45
-        1
-      ];
+      easeOutExpo = quadton 0.16 1 0.3 1;
+      easeOutCirc = quadton 0 0.55 0.45 1;
     };
 
+    animation.animation = komo.hypr.mkAnimations {
+      inputFieldFade = {
+        speed = 5;
+        curve = "easeOutExpo";
+      };
+      inputFieldColor = {
+        speed = 8;
+        curve = "easeOutCirc";
+      };
+    };
+
+    background = {
+      path = toString ./hyprlock-bg.png;
+
+      blur_passes = 1;
+      blur_size = 6;
+    };
+
+    input_field = {
+      monitor = "";
+
+      size = percentXY 10 5;
+      outline_thickness = 5
+
+      
+    }
   };
 }
