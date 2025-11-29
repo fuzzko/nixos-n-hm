2fc9cdf3411b7ad18f4aa5f43e588c1c11a73b83
Komo <afiqquraisyzulkarnain@gmail.com>
2025-11-29 16:12:45 +0800

    changes from gudboye on Sat Nov 29 04:12:45 PM WITA 2025

--- home/options/programs/quickshell/config/shell.nix
+++ home/options/programs/quickshell/config/shell.nix
@@ -1,8 +1,25 @@
 { pkgs ? import <nixpkgs> {}}:
-
+let
+  inherit (pkgs) lib;
+in
 pkgs.mkShellNoCC {
   packages = with pkgs; [
     kdePackages.qtdeclarative
     quickshell
   ];
+
+  env =
+    let
+      makeQmlImportPath = lib.makeSearchPath "lib/qt-6/qml";
+      makeQmlPluginPath = lib.makeSearchPath "lib/qt-6/plugins";
+    in
+    {
+      QML2_IMPORT_PATH = makeQmlImportPath (with pkgs; [
+        "${kdePackages.qtdeclarative}"
+        "${quickshell}"
+      ]);
+      QML_PLUGIN_PATH = makeQmlPluginPath (with pkgs; [
+        "${kdePackages.qtdeclarative}"
+      ]);
+    };
 }
