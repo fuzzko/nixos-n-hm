aa3f4f489333fcacd948ef4464c12dbd3993c5a0
Komo <afiqquraisyzulkarnain@gmail.com>
2025-12-11 01:08:33 +0800

    changes from gudboye on Thu Dec 11 01:08:33 AM WITA 2025

--- nixos/default.nix
+++ nixos/default.nix
@@ -3,22 +3,14 @@ let
   nixpkgs = import npins.nixpkgs { };
   
   nixosSystem =
+    # ugly modified of lib.nixosSystem from the flake
     args:
     import /${npins.nixpkgs}/nixos/lib/eval-config.nix (
       {
         lib = nixpkgs.lib;
-        # Allow system to be set modularly in nixpkgs.system.
-        # We set it to null, to remove the "legacy" entrypoint's
-        # non-hermetic default.
         system = null;
 
         modules = args.modules ++ [
-          # This module is injected here since it exposes the nixpkgs self-path in as
-          # constrained of contexts as possible to avoid more things depending on it and
-          # introducing unnecessary potential fragility to changes in flakes itself.
-          #
-          # See: failed attempt to make pkgs.path not copy when using flakes:
-          # https://github.com/NixOS/nixpkgs/pull/153594#issuecomment-1023287913
           (
             {
               config,
