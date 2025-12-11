51a33410c792d682602094f8b5fb40c464a3a8c1
Komo <afiqquraisyzulkarnain@gmail.com>
2025-12-11 21:43:38 +0800

    changes from gudboye on Thu Dec 11 09:43:37 PM WITA 2025

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -294,4 +294,6 @@ in
 
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = false;
+  services.system76-scheduler.enable = true;
+  environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
 }
