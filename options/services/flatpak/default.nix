19fc7a8b76f670ecded80c35f194332b9cb39202
Komo <afiqquraisyzulkarnain@gmail.com>
2025-12-12 20:47:19 +0800

    changes from gudboye on Fri Dec 12 08:47:18 PM WITA 2025

--- options/services/flatpak/default.nix
+++ options/services/flatpak/default.nix
@@ -1,6 +1,8 @@
 { ... }: {
   services.flatpak.enable = true;
   services.flatpak.packages = [
-    
+    "com.collaboraoffice.Office"
+    "org.vinegarhq.Sober"
+    "com.usebottles.bottles"
   ];
 }
