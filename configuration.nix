94797b16bfa756a2ff7cda016903a0a542c35f4b
Komo <afiqquraisyzulkarnain@gmail.com>
2025-12-12 23:30:43 +0800

    changes from gudboye on Fri Dec 12 11:30:38 PM WITA 2025

--- configuration.nix
+++ configuration.nix
@@ -54,6 +54,7 @@ in
 
     # comment this if something's fucked up
     kernelPackages = pkgs.linuxPackages_zen;
+
     kernelParams = [
       "vt.default_red=30,243,166,249,137,245,148,186,88,243,166,249,137,245,148,166"
       "vt.default_grn=30,139,227,226,180,194,226,194,91,139,227,226,180,194,226,173"
