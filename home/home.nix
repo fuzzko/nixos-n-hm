56f02e34731443f630346da0fc3d9b05646b5cac
Komo <afiqquraisyzulkarnain@gmail.com>
2025-12-12 23:29:59 +0800

    changes from gudboye on Fri Dec 12 11:29:59 PM WITA 2025

--- home/home.nix
+++ home/home.nix
@@ -8,7 +8,7 @@ let
 in
 {
   # A simple business logic to import all configs in ./options, you should check that dir too
-  imports = (komo.filterFilesInDir (x: if (builtins.baseNameOf x) == "default.nix" then builtins.trace "${toString x}\n" true else false) ./options) ++ [
+  imports = (komo.filterFilesInDir (x: (builtins.baseNameOf x) == "default.nix") ./options) ++ [
     ../modules/hm/moor
   ];
 
