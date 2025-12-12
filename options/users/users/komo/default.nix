31133e6ff55c8d22df7f3c1be7062f443997d6ad
Komo <afiqquraisyzulkarnain@gmail.com>
2025-12-12 18:56:13 +0800

    changes from gudboye on Fri Dec 12 06:56:13 PM WITA 2025

---
+++ options/users/users/komo/default.nix
@@ -0,0 +1,15 @@
+{ ... }:
+{
+  users.users.komo = {
+    isNormalUser = true;
+    description = "Komo";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "video"
+      "i2c"
+    ];
+  };
+
+  home-manager.users.komo = x;
+}
