27ddf7b39b0af2eb257ce2f085ebf68d40dc5964
Komo <afiqquraisyzulkarnain@gmail.com>
2025-12-12 23:23:25 +0800

    changes from gudboye on Fri Dec 12 11:23:24 PM WITA 2025

--- options/services/desktopManagers/cosmic/default.nix
+++ options/services/desktopManagers/cosmic/default.nix
@@ -1,6 +1,9 @@
-{...}: {
+{ ... }:
+{
   services.desktopManager.cosmic.enable = true;
+
   services.displayManager.cosmic-greeter.enable = false;
+
   services.system76-scheduler.enable = true;
   environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
 }
