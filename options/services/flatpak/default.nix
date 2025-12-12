086b93f9c8b701d762f569ea2ae10610d80aa911
Komo <afiqquraisyzulkarnain@gmail.com>
2025-12-12 23:37:45 +0800

    changes from gudboye on Fri Dec 12 11:37:45 PM WITA 2025

--- options/services/flatpak/default.nix
+++ options/services/flatpak/default.nix
@@ -1,8 +1,34 @@
-{ ... }: {
+{ ... }:
+{
   services.flatpak.enable = true;
+  services.flatpak.uninstallUnmanaged = true;
+
+  services.flatpak.update = {
+    onActivation = true;
+    auto.enable = true;
+    auto.onCalendar = "weekly";
+  };
+
   services.flatpak.packages = [
     "com.collaboraoffice.Office"
     "org.vinegarhq.Sober"
     "com.usebottles.bottles"
   ];
+
+  services.flatpak.overrides = {
+    global = {
+      Context.sockets = [
+        "wayland"
+        "!x11"
+      ];
+
+      Environment = {
+        # Fix un-themed cursor in some Wayland apps
+        XCURSOR_PATH = "/run/host/user-share/icons:/run/host/share/icons";
+
+        # Force correct theme for some GTK apps
+        GTK_THEME = "Adwaita:dark";
+      };
+    };
+  };
 }
