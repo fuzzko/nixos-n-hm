5bd7908cb90cbe5450c0f52f4f6db7d47e88b239
Komo <afiqquraisyzulkarnain@gmail.com>
2025-12-14 21:54:32 +0800

    changes from gudboye on Sun Dec 14 09:54:31 PM WITA 2025

--- home/options/programs/nushell/default.nix
+++ home/options/programs/nushell/default.nix
@@ -6,11 +6,11 @@ in
   programs.nushell.enable = true;
 
   programs.nushell.configFile.text = ''
-    let npins = r#"${builtins.toJSON npins}"# | from json
     let autoload = $nu.data-dir | path join "vendor/autoload"
 
     mkdir $autoload
     cp ${toString ./autoload}/* $autoload
+
 
     source ${toString ./config.nu}
   '';
