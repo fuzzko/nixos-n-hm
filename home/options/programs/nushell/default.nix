d04f04544ef082a18930be66f329bae3a6d54da2
Komo <afiqquraisyzulkarnain@gmail.com>
2025-12-14 21:40:51 +0800

    changes from gudboye on Sun Dec 14 09:40:51 PM WITA 2025

--- home/options/programs/nushell/default.nix
+++ home/options/programs/nushell/default.nix
@@ -1,7 +1,12 @@
-{ ... }: {
+{ ... }:
+let
+  npins = import ../../../../npins;
+in
+{
   programs.nushell.enable = true;
 
   programs.nushell.configFile.text = ''
+    let npins = r#"${builtins.toJSON npins}"# | from json
     let autoload = $nu.data-dir | path join "vendor/autoload"
 
     mkdir $autoload
