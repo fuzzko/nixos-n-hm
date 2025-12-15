5fd35496ea1ccc6bcbeb8f05cb9fc3f5f4561687
Komo <afiqquraisyzulkarnain@gmail.com>
2025-12-15 22:45:33 +0800

    changes from gudboye on Mon Dec 15 10:45:33 PM WITA 2025

--- home/home.nix
+++ home/home.nix
@@ -6,10 +6,6 @@ let
   komoLib = import ../lib pkgs.lib;
 in
 {
-  # A simple business logic to import all configs in ./options, you should check that dir too
-  imports = (komoLib.filterFilesInDir (x: (builtins.baseNameOf x) == "default.nix") ./options) ++ [
-    ../modules/hm/moor
-  ];
 
   home.stateVersion = "24.05";
 }
