eb70110320a706ccebfe3912b09de09da1971325
Komo <afiqquraisyzulkarnain@gmail.com>
2025-12-18 02:44:07 +0800

    changes from gudboye on Thu Dec 18 02:44:07 AM WITA 2025

--- home/options/_cosmic/default.nix
+++ home/options/_cosmic/default.nix
@@ -1,8 +1,14 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 let
   homeLib = config.lib;
-  komoLib = homeLib.komo;
 in
 {
   xdg.configFile."cosmic".source = homeLib.file.mkOutOfStoreSymlink (toString ./config);
+
+  home.packages = with pkgs; [
+    cosmic-ext-applet-external-monitor-brightness
+    cosmic-ext-applet-privacy-indicator
+    cosmic-ext-applet-caffeine
+    cosmic-ext-applet-minimon
+  ];
 }
