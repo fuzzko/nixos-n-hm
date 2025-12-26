dc5738716a0ac8a8d124d96028be88520cd35fdf
Komo <afiqquraisyzulkarnain@gmail.com>
2025-12-26 10:03:47 +0800

    changes from gudboye on Fri Dec 26 10:03:47 AM WITA 2025

---
+++ modules/hm/zen-browser/default.nix
@@ -0,0 +1,27 @@
+{ pkgs, lib, npins, ... }:
+let
+  makeFirefoxModule = import "${npins.home-manager}/modules/programs/firefox/mkFirefoxModule.nix";
+
+  module = makeFirefoxModule {
+    modulePath = "programs.zen-browser";
+    wrappedPackage = "zen-browser";
+    visible = true;
+
+    platforms.linux.configPath = ".zen";
+  };
+in
+{
+  imports = [
+    module
+  ];
+
+  config = {
+    assertions = [
+      (lib.hm.assertPlatform "programs.zen-browser" pkgs lib.platforms.linux)
+      {
+        assertion = pkgs ? zen-browser && pkgs ? zen-browser-unwrapped;
+        message = ''programs.zen-browser "package" option require pkgs.zen-browser to be available'';
+      }
+    ];
+  };
+}
