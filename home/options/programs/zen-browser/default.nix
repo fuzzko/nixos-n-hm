2c2314ba04eddb153e0cc29ae47515fb1183877c
Komo <afiqquraisyzulkarnain@gmail.com>
2025-12-26 10:12:59 +0800

    changes from gudboye on Fri Dec 26 10:12:59 AM WITA 2025

---
+++ home/options/programs/zen-browser/default.nix
@@ -0,0 +1,18 @@
+{ npins, lib, ... }:
+{
+  programs.zen-browser.enable = true;
+  programs.zen-browser.profiles.default = {
+    preConfig = lib.readFile "${npins.betterfox}/user.js";
+
+    extraConfig = ''
+      /** SmoothFox **/
+
+      user_pref("apz.overscroll.enabled", true); // DEFAULT NON-LINUX
+      user_pref("general.smoothScroll", true); // DEFAULT
+      user_pref("mousewheel.default.delta_multiplier_y", 275); // 250-400; adjust this number to your liking
+      // Firefox Nightly only:
+      // [1] https://bugzilla.mozilla.org/show_bug.cgi?id=1846935
+      user_pref("general.smoothScroll.msdPhysics.enabled", false); // [FF122+ Nightly]
+    '';
+  };
+}
